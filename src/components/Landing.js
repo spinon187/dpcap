4e66259968ed87d1fc75a95f6b3f9fad503860da
spinon187 <spinon187@gmail.com>
2020-02-23 18:53:53 -0600

    will this work???

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import ctav1 from '../img/ctav1.svg';
+import cta from '../img/cta.svg';
 
 const Landing = props => {
   return(
     <LandingBox>
-      <img src={ctav1} alt='Join the DPCAP!' className='cta'/>
+      <img src={cta} alt='Join the DPCAP!' className='cta'/>
     </LandingBox>
   )
 }
 
 const LandingBox = styled.div`
+  width: 98vw;
   img {
     width: 100%
   }
